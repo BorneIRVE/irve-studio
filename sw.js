ea990a0880c283c6dd4d7138e7d3163d94f629fe
BorneIRVE <lucaswartelles7@gmail.com>
2026-06-11 12:47:53 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker VoltExpert CRM
-const CACHE = 'voltexpert-v19';
+const CACHE = 'voltexpert-v20';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
