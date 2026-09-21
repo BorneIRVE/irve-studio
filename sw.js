6a411e248897539ec3417a16a7879d4eedcc79d6
BorneIRVE <lucaswartelles7@gmail.com>
2026-09-21 17:31:32 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker AEROHM CRM
-const CACHE = 'voltexpert-v40';
+const CACHE = 'voltexpert-v46';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
